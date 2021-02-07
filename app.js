3abf04c34d168444b628c29260cfb2de486571b6
Sam England <77686863+sam-eng11@users.noreply.github.com>
2021-02-07 04:47:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res) {
+ res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res){
 
   //url is too long so make a const.
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=2d2dadb9aad8c64624242861cb3f681e&units=imperial"
+  const query = req.body.cityName;
+  const apiKey = "2d2dadb9aad8c64624242861cb3f681e";
+  const unit = "imperial";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid="+apiKey+"&units="+unit+""
   https.get(url, function(response) {
     console.log(response.statusCode);
 
@@ -17,7 +27,7 @@ app.get("/", function(req, res) {
       const icon = weatherData.weather[0].icon;
       const imageURL = "http://openweathermap.org/img/wn/"+ icon +"@2x.png";
       res.write("<p>The weather is currently " + desc + "</p>");
-      res.write("<h1>The temperature in London is " + temp + " degrees F.</h1>");
+      res.write("<h1>The temperature in "+query+" is " + temp + " degrees F.</h1>");
       res.write("<img src = " + imageURL + ">");
       res.send();
       console.log(temp);
@@ -25,6 +35,8 @@ app.get("/", function(req, res) {
     });
   });
 });
+
+
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
